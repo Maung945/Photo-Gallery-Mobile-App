4453cd452f727bd39f4e9e0e382a48a95d7aece2
Myo Aung <myo.aung8091@gmail.com>
2023-10-17 18:16:46 -0700

    Retrofit, Fetching JSON from Flicker and Moshi

--- app/src/main/java/api/FlickerApi.kt
+++ app/src/main/java/api/FlickerApi.kt
@@ -2,7 +2,16 @@ package api
 
 import retrofit2.http.GET
 
+private const val API_KEY = "0924306ff4ca3c2971a983833d1c2230"
+
 interface FlickrApi {
-    @GET("/")
-    suspend fun fetchContents(): String
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=$API_KEY" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    //suspend fun fetchPhotos(): String
+    suspend fun fetchPhotos(): FlickrResponse
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -6,13 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
 import api.FlickrApi
 import com.bignerdranch.android.photogallery.databinding.FragmentPhotoGalleryBinding
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+//import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.create
 
 private const val TAG = "PhotoGalleryFragment"
@@ -23,6 +26,8 @@ class PhotoGalleryFragment : Fragment() {
         get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
+    private val photoGalleryViewModel: PhotoGalleryViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -36,15 +41,13 @@ class PhotoGalleryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.flickr.com/")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
-        val flickrApi: FlickrApi = retrofit.create<FlickrApi>()
 
         viewLifecycleOwner.lifecycleScope.launch {
-            val response = flickrApi.fetchContents()
-            Log.d(TAG, "Response received: $response")
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                photoGalleryViewModel.galleryItems.collect { items ->
+                    Log.d(TAG, "Response received: $items")
+                }
+            }
         }
 
     }
